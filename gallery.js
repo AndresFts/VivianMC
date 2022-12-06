e557fcdbdf3524c31c96125325221685200c268b
AndresFts <76638493+AndresFts@users.noreply.github.com>
2022-12-06 22:21:19 +0100

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -1,4 +1,5 @@
 const projects = document.querySelectorAll(".g_images");
+const texts = document.querySelectorAll(".g_text");
 const nextbtn = document.getElementById("nextbtn")
 let i = 0;
 
@@ -6,14 +7,17 @@ nextbtn.onclick = next;
 
 function next()
 {
+    texts[i].classList.remove("active");
     projects[i].classList.remove("active");
-    if (i == projects.length - 1)
-    {
-        i = 0;
-    }
-    else
+
+    if (i < projects.length-1)
     {
         ++i;
     }
+    else
+    {
+        i=0;
+    }
+    texts[i].classList.add("active");
     projects[i].classList.add("active");
 }
